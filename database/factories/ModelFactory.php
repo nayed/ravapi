6a04c3ae023c7c465439d613663ad594922cca03
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-07-03 23:59:11 +0200

    factorie

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,14 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'name' => $faker->word
+    ];
+});
+
+$factory->define(App\Description::class, function (Faker\Generator $faker) {
+    return [
+        'body' => $faker->text
     ];
 });
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions; // <= trait
     /**
      * A basic functional test example.
      *
@@ -19,7 +20,25 @@ class ExampleTest extends TestCase
 
     public function testProductsList()
     {
+        $products = factory(App\Product::class, 3)->create();
+
         $this->get(route('api.products.index'))
              ->assertResponseOk();
+
+        array_map(function($product) {
+            $this->seeJson($product->jsonSerialize());
+        }, $products->all());
+    }
+
+    public function testProductDescriptionList()
+    {
+        $product = factory(\App\Product::class)->create();
+        $product->descriptions()->saveMany(factory(\App\Description::class, 3)->make());
+
+        $this->get(route('api.products.descriptions.index', ['products' => $product->id]));
+
+        array_map(function($product) {
+            $this->seeJson($product->jsonSerialize());
+        }, $product->descriptions->all());
     }
 }
